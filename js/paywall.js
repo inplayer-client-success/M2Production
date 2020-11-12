afe9abff514d2cb019e7dcf6b3af57c45c0a6aab
martinaveljanovska <martina.veljanovska@gmail.com>
2020-11-12 15:10:11 +0100

    updated to v3 paywall

--- js/paywall.js
+++ js/paywall.js
@@ -5,6 +5,29 @@ var config = {
     service_url: "https://services.inplayer.com"
 }
 
+window.addEventListener('load', (event) => {
+    var paywall = new InplayerPaywall('cc0cb013-9b32-4e3d-a718-8f9b69d49908', [{
+        id: getParameterByName('id')
+      }]);
+    var paywall = new InplayerPaywall('cc0cb013-9b32-4e3d-a718-8f9b69d49908', []);
+    $(".inplayer-paywall-login").click(function () {
+        paywall.showPaywall({
+            asset: {}
+        });
+    });
+    $(".inplayer-paywall-logout").hide();
+    paywall.on("authenticated", function () {
+      $(".inplayer-paywall-login").hide();
+      $(".inplayer-paywall-logout").show();
+    });
+    paywall.on('inject', function () {
+        $(".inplayer-paywall").addClass('responsive-iframe');
+    });
+});
+
+var paywall = new InplayerPaywall('cc0cb013-9b32-4e3d-a718-8f9b69d49908', [{
+    id: getParameterByName('id')
+  }]);
 
 // CREATE PACKAGE ASSET
 function createItemElement(assetId, assetPhoto, assetTitle) {
